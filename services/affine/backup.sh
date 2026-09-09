944fece0fa7dcb858aaaa644e2abcc9af5c268ff
Lievens, Karel <lvnskarel@gmail.com>
2026-09-09 06:58:53 +0200

    chore: [Affine] remove service

--- services/affine/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-. "$(dirname "$(readlink -f "$0")")/../../scripts/lib/backup-lib.sh"
-
-set -a; . ./.env; set +a
-dump_postgres postgres "${DB_USERNAME}" "${DB_DATABASE}"
-
